a3d6aa98b8182e2f3860b2bced791e676af49b4b
Mikołaj Cieszczyk <mikolaj.cieszczyk@gmail.com>
2025-01-18 13:38:28 +0100

    master: fixed page base to fix blind links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mc-blog.github.io",
+  site: "https://mikolajcieszczyk.github.io",
+  base: "/",
 });
